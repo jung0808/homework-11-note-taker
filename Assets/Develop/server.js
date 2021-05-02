856611d8566c1bff9fec7f2606d244756323c8ba
Jung Nam <j.nam0808@gmail.com>
2021-05-02 17:20:52 -0400

    adding more content

---
+++ Assets/Develop/server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
