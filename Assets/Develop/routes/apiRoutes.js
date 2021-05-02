d1a25e48d389323bb04cee457a407c63561dfb44
Jung Nam <j.nam0808@gmail.com>
2021-05-02 18:13:45 -0400

    working on post method

--- Assets/Develop/routes/apiRoutes.js
+++ Assets/Develop/routes/apiRoutes.js
@@ -2,8 +2,13 @@ const router = require("express").Router();
 const notes = require("../db/db.json");
 
 router.get("/notes", function (req, res) {
-  console.log("message");
   res.json(notes);
+});
+
+router.post("/notes", function (req, res) {
+  // res.json(notes);
+  console.log(req);
+  req.body;
 });
 
 module.exports = router;
